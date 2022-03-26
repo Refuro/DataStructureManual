ad70162437b3b6360cda46d050fc9c259b2d2c07
Refuro <61066621+Refuro@users.noreply.github.com>
2022-03-26 20:58:53 +0000

    Added basic hash table functionality and some new comments

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//Christian Spencer, 3/26/22
+
+//My array implementation, since Javascript does not support overloading the constructor, I haven't allowed a delcaration with parameters yet, but plan to figure out a solution when I have more time
 class myArray{
   constructor(){
     this.length = 0;
@@ -39,11 +42,43 @@ class myArray{
   }
 }
 
+
+//My hashtable implementation, I'm not using my own array class here because I have not yet implemented size declaration, but plan to switch it after I implement that
 class myHashTable{
   constructor(size){
-    this.size = size;
-
+    this.data = new Array(size);
   }
+//This is the primary hash function
+  _hash(key){
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % size;
+    }
+    return hash;
+  }
+  //Sets a key and value
+  set(key, value){
+    this.address = this._hash(key);
+    if(!this.data[this.address]){
+      this.data[this.address] = [];
+    }
+    this.data[this.address] = [key,value];
+    return;
+  }
+  //Gets the value at the given key
+  get(key){
+    
+  }
+  //Returns an array of all the hash table contents, o(n^2), could try to improve this later
+  keys(){
+    
+  }
+  
+}
+
+function testMyHashTable(){
+  let testHash = new myHashTable(10);
+  console.log(testHash);
 }
 
 function testMyArray() {
@@ -61,3 +96,5 @@ function testMyArray() {
   testArray.delete(1);
   console.log(testArray);       //Expected, Length: 2, data {First, Final}
 }
+
+testMyHashTable();
